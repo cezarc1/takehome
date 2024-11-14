df7c9a793d436f49bcf033bcdf3071a33d6fbc8a
Cezar Cocu <cezarc1@users.noreply.github.com>
2024-11-13 19:05:31 -0500

    pass the trainset to the knn optimizer during compilation.

--- brain/modules/knn_optimizer.py
+++ brain/modules/knn_optimizer.py
@@ -9,9 +9,11 @@ class KNNOptimizerModule(Module):
 
     def __init__(self, examples: List[LabeledChatHistory], k: int = 3):
         super().__init__()
-        training_examples = [example.to_dspy_example() for example in examples]
+        self.training_examples = [
+            example.to_dspy_example() for example in examples
+        ]
         # TODO: Add metric and split for test sets/
-        self.optimizer = KNNFewShot(k, trainset=training_examples)
+        self.optimizer = KNNFewShot(k, trainset=self.training_examples)
 
     def compile_module(self, module: Module) -> Module:
-        return self.optimizer.compile(module)
+        return self.optimizer.compile(module, trainset=self.training_examples)
